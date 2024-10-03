ee6ef463c2393c9ee6521df563b2777c7615843e
LTexocotitlaP <ltexocotitlap2300@alumno.ipn.mx>
2024-10-03 01:08:47 -0600

    feat: add treesitter to the config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -23,4 +23,19 @@ require("lazy").setup({
             vim.cmd.colorscheme("kanagawa-dragon")
         end,
     },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup ({
+                ensure_installed = {"c", "lua", "vim", "vimdoc", "query"}, 
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                },
+            })
+        end,
+    },
+    {
+        "github/copilot.vim",
+    }
 })
