cc6c5462fee8b99dca6e78bf648d820a3674f573
LTexocotitlaP <ltexocotitlap2300@alumno.ipn.mx>
2024-10-02 19:17:52 -0600

    feat(opt): Added chapter 2 options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
+-- Chapter 1 configuration
 vim.opt.number = true
 vim.opt.relativenumber = true
 
@@ -8,8 +9,21 @@ vim.opt.wrap = false
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
+
+-- Chapter 2 configuration fix for ident
 vim.opt.shiftwidth = 4
+-- Chapter 2 configuration fix for ident
 
 vim.opt.clipboard = "unnamedplus"
 
 vim.opt.scrolloff = 999
+
+-- Chapter 2 configuration 
+
+vim.opt.virtualedit = "block" -- ctrl + v allows to get edit mode with block option
+
+vim.opt.inccommand = "split" -- %s/
+
+vim.opt.ignorecase = true -- evicts upper case convention for plugins
+
+vim.opt.termguicolors = true
