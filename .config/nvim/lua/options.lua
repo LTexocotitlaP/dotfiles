043021f53dec0e32514218223b1a5867620b4d00
LTexocotitlaP <ltexocotitlap2300@alumno.ipn.mx>
2024-10-02 20:29:23 -0600

    fix(stdpath): chapter 3 fix stdpath bugs

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,29 @@
+-- Chapter 1 configuration
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+
+-- Chapter 2 configuration fix for ident
+vim.opt.shiftwidth = 4
+-- Chapter 2 configuration fix for ident
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 999
+
+-- Chapter 2 configuration 
+
+vim.opt.virtualedit = "block" -- ctrl + v allows to get edit mode with block option
+
+vim.opt.inccommand = "split" -- %s/
+
+vim.opt.ignorecase = true -- evicts upper case convention for plugins
+
+vim.opt.termguicolors = true
